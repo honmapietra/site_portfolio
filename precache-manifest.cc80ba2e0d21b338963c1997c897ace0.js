38d1edab33cfdff8dba5136294589fdb013eb7b8
honmapietra <54584172+honmapietra@users.noreply.github.com>
2020-09-11 14:31:24 -0300

    Updates

---
+++ precache-manifest.cc80ba2e0d21b338963c1997c897ace0.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4ed5869077cdd9d6493bbe480693c09",
+    "url": "/site_portfolio/index.html"
+  },
+  {
+    "revision": "13264ebcae2443312858",
+    "url": "/site_portfolio/static/css/main.5e8340a8.chunk.css"
+  },
+  {
+    "revision": "52232dd952fe44a86766",
+    "url": "/site_portfolio/static/js/2.be067f04.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/site_portfolio/static/js/2.be067f04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "13264ebcae2443312858",
+    "url": "/site_portfolio/static/js/main.1914faea.chunk.js"
+  },
+  {
+    "revision": "96ee0f23332392f55439",
+    "url": "/site_portfolio/static/js/runtime-main.dfe273c9.js"
+  },
+  {
+    "revision": "74072a9b3b85950020651c109962c0c4",
+    "url": "/site_portfolio/static/media/dudaicon.74072a9b.jpg"
+  },
+  {
+    "revision": "1c0fab30bfad367ffd2572eeb2ee2ae6",
+    "url": "/site_portfolio/static/media/pintura1.1c0fab30.jpg"
+  },
+  {
+    "revision": "29e9bff682547e0b9bc49a16e40543e8",
+    "url": "/site_portfolio/static/media/pintura2.29e9bff6.jpg"
+  },
+  {
+    "revision": "ec804fda6a87eb8883b51907dc050d64",
+    "url": "/site_portfolio/static/media/pintura3.ec804fda.jpg"
+  },
+  {
+    "revision": "2cd0461b796155e0fee2e32afa6af489",
+    "url": "/site_portfolio/static/media/pintura4.2cd0461b.jpg"
+  },
+  {
+    "revision": "1916fd566796615291390e0743411fdb",
+    "url": "/site_portfolio/static/media/pintura5.1916fd56.jpg"
+  },
+  {
+    "revision": "0b6e6dcb750050589563ef0679634cd0",
+    "url": "/site_portfolio/static/media/pintura6.0b6e6dcb.jpg"
+  },
+  {
+    "revision": "5c56739e24fd7590366824b24c3ce3cf",
+    "url": "/site_portfolio/static/media/pintura7.5c56739e.jpg"
+  },
+  {
+    "revision": "1b5bcc3bb92156f534b9e35e98875f44",
+    "url": "/site_portfolio/static/media/pintura8.1b5bcc3b.jpg"
+  }
+]);
